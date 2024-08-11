cddde86e225d60059c02fc78e514296dfea2963b
sunq945 <sunq8964@gmail.com>
2024-08-12 05:06:30 +0800

    Update serv00_vmess.sh

--- vmess/serv00_vmess.sh
+++ vmess/serv00_vmess.sh
@@ -34,12 +34,12 @@ else
 fi
 
 
-read_xray_port() {
+read_vmess_port() {
     while true; do
-        reading "请输入服务器端口 (面板开放的tcp端口): " xray_port
-        if [[ "$xray_port" =~ ^[0-9]+$ ]] && [ "$xray_port" -ge 1 ] && [ "$xray_port" -le 65535 ]; then
-            green "你的frps端口为: $xray_port"
-            export XRAY_PORT=$xray_port
+        reading "请输入服务器端口 (面板开放的tcp端口): " vmess_port
+        if [[ "$vmess_port" =~ ^[0-9]+$ ]] && [ "$vmess_port" -ge 1 ] && [ "$vmess_port" -le 65535 ]; then
+            green "你的frps端口为: $vmess_port"
+            export PORT=$vmess_port
             break
         else
             yellow "输入错误，请重新输入面板开放的TCP端口"
@@ -48,7 +48,7 @@ read_xray_port() {
 }
 
 
-install_xray() {
+install_vmess() {
 echo -e "${yellow}本脚本只安装frps穿透代理服务器和启动脚本${re}"
 echo -e "${yellow}开始运行前，请确保在面板${purple}已开放1个tcp端口${re}"
 echo -e "${yellow}面板${purple}Additional services${yellow}中的${purple}Run your own applications${yellow}已开启为${purplw}Enabled${yellow}状态${re}"
@@ -56,21 +56,21 @@ reading "\n确定继续安装吗？【y/n】: " choice
   case "$choice" in
     [Yy])
         cd $WORKDIR
-        read_xray_port     
+        read_vmess_port     
         download_xray_core && wait
         generate_config        
-        run_xray && sleep 3   
+        run_vmess && sleep 3   
       ;;
     [Nn]) exit 0 ;;
     *) red "无效的选择，请输入y或n" && menu ;;
   esac
 }
 
-uninstall_xray() {
+uninstall_vmess() {
   reading "\n确定要卸载吗？【y/n】: " choice
     case "$choice" in
        [Yy])
-          ps aux | grep "xray" | grep -v grep | awk '{print $2}' | xargs kill -9
+          pgrep -f "vmess_config.json"| grep -v grep | awk '{print $2}' | xargs kill -9
           rm -rf $WORKDIR
            echo -e "${green} 卸载完成 ${re}"
           ;;
@@ -90,23 +90,23 @@ reading "\n清理所有进程将退出ssh连接，确定继续清理吗？【y/n
 
 # Download Dependency Files
 download_xray_core() {
-  local app_name="Xray-freebsd-64.zip"
-  echo -e "${yellow} 下载xray-core( $app_name ): ${re}"
-  curl -sL -o $app_name https://github.com/XTLS/Xray-core/releases/latest/download/Xray-freebsd-64.zip
-  if [ -f "./$app_name" ];then
-    echo -e "${green} 下载 $app_name 成功，正在解压...${re}"
-    unzip -o $app_name | awk 'BEGIN {ORS=" "} {print "."}'  
+  local file_name="Xray-freebsd-64.zip"
+  echo -e "${yellow} 下载xray-core( $file_name ): ${re}"
+  curl -sL -o $file_name https://github.com/XTLS/Xray-core/releases/latest/download/Xray-freebsd-64.zip
+  if [ -f "./$file_name" ];then
+    echo -e "${green} 下载 $file_name 成功，正在解压...${re}"
+    unzip -o $file_name | awk 'BEGIN {ORS=" "} {print "."}'  
     echo -e "${green} 解压完毕${re}"
     chmod +x xray
-    rm -rf $app_name
-    echo -e "${yellow} $app_name 已删除.${re}"
+    rm -rf $file_name
+    echo -e "${yellow} $file_name 已删除.${re}"
   else
-    echo -e "${red} 下载 $app_name 失败 ${re}"
+    echo -e "${red} 下载 $file_name 失败 ${re}"
     exit 1
  fi
 
   echo -e "${yellow}正在下载generator.js${re}"
-  curl -fsSL  https://raw.githubusercontent.com/sunq945/serv00-app/main/xray/generator.js -o generator.js 
+  curl -fsSL  https://raw.githubusercontent.com/sunq945/serv00-app/main/vmess/generator.js -o generator.js 
   
   if [ -f "./generator.js" ];then
     echo -e "${green} 下载 generator.js 成功${re}"
@@ -119,14 +119,14 @@ download_xray_core() {
 download_check_script(){
   local path=$(pwd)
   cd $WORKDIR
-  curl -fsSL  https://raw.githubusercontent.com/sunq945/serv00-app/main/xray/checkxray.sh -o checkxray.sh && chmod +x checkxray.sh 
-  if [ -f "./checkxray.sh" ];then
-    echo -e "${green} 下载 checkxray.sh 成功， 文件位置： ${purple}"$WORKDIR/checkxray.sh" ${re}"
+  curl -fsSL  https://raw.githubusercontent.com/sunq945/serv00-app/main/vmess/checkvmess.sh -o checkvmess.sh && chmod +x checkvmess.sh
+  if [ -f "./checkvmess.sh" ];then
+    echo -e "${green} 下载 checkvmess.sh 成功， 文件位置： ${purple}"$WORKDIR/checkvmess.sh" ${re}"
     echo -e "${yellow} 你可以在vps的面板上找到cron job,进去之后 点击 “ Add cron job” 添加定时任务，建议定时为3分钟（Minuts填Every 和 3 ，其他时间选项填Each Time）， 命令行填写:
-    ${green}/bin/sh $WORKDIR/checkxray.sh
+    ${green}/bin/sh $WORKDIR/checkvmess.sh
     ${re}"
   else
-    echo -e "${red} 下载checkxray.sh失败,请重新下载 ${re}"
+    echo -e "${red} 下载checkvmess.sh失败,请重新下载 ${re}"
     exit 1
   fi
   cd $path
@@ -139,7 +139,7 @@ generate_config() {
 {
     "server":{        
         "uuid": "$UUID",
-        "port":"$XRAY_PORT",
+        "port":"$PORT",
         "host":"$MYDOMAIN"
     }
 }
@@ -150,19 +150,19 @@ node generator.js
 }
 
 show_link(){
-  cat $WORKDIR/xray_link.txt 
+  cat $WORKDIR/vemss_link.txt 
   echo -e "\n"
   echo -e "\n"
 }
 # running files
-run_xray() { 
-  if [ -f "./xray" ] && [ -f "./config.json" ]; then
-        ps aux | grep "xray" | grep -v grep | awk '{print $2}' | xargs kill -9        
-        nohup ./xray -c config.json >/dev/null 2>&1 &
+run_vmess() { 
+  if [ -f "./xray" ] && [ -f "./vmess_config.json" ]; then
+        pgrep -f "vmess_config.json" | grep -v grep | awk '{print $2}' | xargs kill -9               
+        nohup ./xray -c vmess_config.json >/dev/null 2>&1 &
         sleep 3
-        pgrep -f "xray" > /dev/null && green "xray is running" || { red "xray is not running, failed!" ;}
+        pgrep -f "vmess_config.json"> /dev/null && green "vmess is running" || { red "vmess is not running, failed!" ;}
   else
-        purple "xray or config.json is not exist,skiping runing"
+        purple "xray or vmess_config.json is not exist,skiping runing"
   fi
 }
 
@@ -172,16 +172,16 @@ menu() {
    clear
    #while true;do
    echo ""
-   purple "============ xray 一键安装脚本 =======\n"
-   echo -e "${green}脚本地址：${re}${yellow}https://github.com/sunq945/serv00-app/tree/main/xray${re}\n"
+   purple "============ vmess 一键安装脚本 =======\n"
+   echo -e "${green}脚本地址：${re}${yellow}https://github.com/sunq945/serv00-app/tree/main/vmess${re}\n"
    purple "转载请注明出处，请勿滥用\n"
-   green "1. 安装xray"
+   green "1. 安装vmess"
    echo  "==============="
-   red "2. 卸载xray"
+   red "2. 卸载vmess"
    echo  "==============="
    green "3. 查看节点信息"
    echo  "==============="
-   yellow "4. 下载检测脚本checkxray.sh"
+   yellow "4. 下载检测脚本checkvmess.sh"
    echo  "==============="
    yellow "5. 清理所有进程"
    echo  "==============="
@@ -190,8 +190,8 @@ menu() {
    reading "请输入选择(0-5): " choice
    echo ""
     case "${choice}" in
-        1) install_xray ;;
-        2) uninstall_xray ;; 
+        1) install_vmess ;;
+        2) uninstall_vmess ;; 
         3) show_link ;; 
         4) download_check_script ;;
         5) kill_all_tasks ;;
